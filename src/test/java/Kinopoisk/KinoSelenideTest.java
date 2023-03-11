f53825a69dec62c0fd3ac245e88f84d3e5834f9f
Costa Varyn <d3tug@yandex.ru>
2023-03-11 08:51:21 +0300

    add tests

--- src/test/java/Kinopoisk/KinoSelenideTest.java
+++ src/test/java/Kinopoisk/KinoSelenideTest.java
@@ -1,24 +1,30 @@
 package Kinopoisk;
 
 import Base.BaseSelenideTest;
-import com.codeborne.selenide.Condition;
+import Base.SearchPage;
+import Base.SearchResultPage;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$$x;
-import static com.codeborne.selenide.Selenide.$x;
-
 public class KinoSelenideTest extends BaseSelenideTest {
-    
+    private final static String URL = "https://www.kinopoisk.ru/s/";
     @Test
     public void checkFilms() {
-        Selenide.open("https://www.kinopoisk.ru/s/");
-        $x("//*[@id='find_film']").sendKeys("Сумерки");
-        $x("//form[@id='formSearchMain']//input[@value='поиск']").click();
-        $$x("//div[@class='search_results search_results_last']//p[@class='name']/a")
-                .forEach(x-> System.out.println(x.getText()));
-        $x("//div[@class='element most_wanted']//span[@class='year']")
-                .shouldHave(Condition.text("2008"));
+        Selenide.open(URL);
+        SearchPage searchPage = new SearchPage();
+        searchPage.setFindValue("Сумерки");
+        SearchResultPage searchResultPage = searchPage.clickSearch();
+        searchResultPage.getAllTitle();
+        searchResultPage.checkMovieDate("2008");
+    }
 
+    @Test
+    void checkUniqueMovie() {
+        Selenide.open(URL);
+        SearchPage searchPage = new SearchPage();
+        searchPage.setFindValue("Весна");
+        SearchResultPage searchResultPage = searchPage.clickSearch();
+        searchResultPage.getAllTitle();
+        searchResultPage.checkMovieDate("2014");
     }
 }
