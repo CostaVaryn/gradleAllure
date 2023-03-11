b0c4b7c531b57debebd3c6588bbde19c22df15c5
Costa Varyn <d3tug@yandex.ru>
2023-03-11 08:50:07 +0300

    refactor

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -12,48 +12,48 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected static WebDriver driver;
-    private static final int TIMOUT = 15;
+    private static final int TIMEOUT = 15;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\cosva\\IdeaProjects\\gradleAllure\\src\\main\\resources\\chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         // chromeOptions.addArguments("--headless");
         driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(TIMOUT, TimeUnit.SECONDS);
-        driver.manage().timeouts().pageLoadTimeout(TIMOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(TIMEOUT, TimeUnit.SECONDS);
     }
 
     /**
      *  @AfterEach
-     *   public void tearDown() {
+     *   public void tearDown(){
      *       driver.close();
      *       driver.quit();
      * }
      */
 
-    public static void switchTo(int window) {
+    public static void switchTo(int window){
         Set<String> tabs = driver.getWindowHandles();
         ArrayList<String> tabsArray = new ArrayList<>(tabs);
         driver.switchTo().window(tabsArray.get(window));
     }
 
-    public static void switchTo(String pagePath) {
+    public static void switchTo(String pagePath){
         Set<String> tabs = driver.getWindowHandles();
         for(String tab : tabs){
             driver.switchTo().window(tab);
-            if(driver.getCurrentUrl().startsWith(pagePath)) {
+            if(driver.getCurrentUrl().startsWith(pagePath)){
                 return;
             }
         }
         throw new RuntimeException("Вкладка не найдена");
     }
 
-    public static boolean checkCookieContains(String key, String value) {
+    public static boolean checkCookieContains(String key, String value){
         return driver.manage().getCookieNamed(key).getValue().contains(value);
     }
 
-    public static boolean checkCookieEquals(String key, String value) {
+    public static boolean checkCookieEquals(String key, String value){
         return driver.manage().getCookieNamed(key).getValue().equals(value);
     }
 
